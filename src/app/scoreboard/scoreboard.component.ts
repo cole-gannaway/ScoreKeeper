6f80b788706f953e1f5195dd2cd02b5f4a0bca8e
ctgannaway <ctgannaway@crimson.ua.edu>
2019-08-04 23:23:49 -0400

    updates

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -9,9 +9,10 @@ import { Player } from '../../player';
 })
 export class ScoreboardComponent implements OnInit {
   players: Array<Player>;
-
+  round: Number;
   constructor() { 
     this.players = new Array<Player>();
+    this.round = 7;
   }
 
   ngOnInit() {
@@ -23,7 +24,15 @@ export class ScoreboardComponent implements OnInit {
     }
     console.log(this.players);
   }
-
+  sortPlayersByScore(){
+    console.log('About to sort');
+    this.players = this.players.sort(Player.getComparatorScores());
+  }
+  addPlayer(){
+    var createdPlayer:Player = new Player();
+    createdPlayer.setName('NewPlayer');
+    this.players.push(createdPlayer);
+  }
   deletePlayer(i:Number){
     console.log('i is ' + i);
     this.players.splice(Number(i), 1);
@@ -32,5 +41,49 @@ export class ScoreboardComponent implements OnInit {
     var player = this.players[i];
     player.score = Number(player.score) + Number(10);
   }
-
+  gotJeopardyCall(i){
+    var player = this.players[i];
+    player.score = Number(player.score) + Number(player.wager);
+  }
+  missJeopardyCall(i){
+    var player = this.players[i];
+    player.score = Number(player.score) - Number(player.wager);
+  }
+  debugPlayers(){
+    console.log(this.players);
+  }
+  changeRound(dir){
+    if (dir == 'plus'){
+      this.round = Number(this.round) + 1;
+    }
+    else if (dir == 'minus'){
+      this.round = Number(this.round) - 1;
+    }
+    else {
+      console.error('Invalid value of dir = ' + dir);
+    }
+    // reset bids
+    for (var p of this.players){
+      p.bid = 0;
+    }
+  }
+  toggleJeopardyMode(){
+    var jeopardy = document.getElementById('JeopardyModeTable');
+    var defTable = document.getElementById('DefaultTable');
+    if (jeopardy.hidden){
+      defTable.hidden = true;
+      jeopardy.hidden = false;
+    }
+    else {
+      defTable.hidden = false;
+      jeopardy.hidden = true;
+    }
+  }
+  toggleWagerInput(event,i){
+    // var newClassName = Player.getInitialWagerClassName();
+    // if (event.source.checked){
+    //   newClassName = 'key';
+    // }
+    // this.players[i].setWagerClassName(newClassName);
+  }
 }
